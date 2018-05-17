965fb555b069a6cb17b65b1a05753515b66ac129
Ochodus <mario1470@naver.com>
2018-05-17 16:43:31 +0900

    solve indent problem

--- add_todo.py
+++ add_todo.py
@@ -1,16 +1,16 @@
 import sqlite3
 
 def add_todo():
-	conn = sqlite3.connect("ace.db")
+    conn = sqlite3.connect("ace.db")
 
-	cur = conn.cursor()
+    cur = conn.cursor()
 
-	t = input("Todo? ")
-	d = input("Due date? ")
-	data = ((t, d), )
-	sql = "insert into todo (what, due, finished) values (?, ?, 0);"
+    t = input("Todo? ")
+    d = input("Due date? ")
+    data = ((t, d), )
+    sql = "insert into todo (what, due, finished) values (?, ?, 0);"
 
-	cur.executemany(sql, data)
-	conn.commit()
+    cur.executemany(sql, data)
+    conn.commit()
 
-	conn.close()
+    conn.close()

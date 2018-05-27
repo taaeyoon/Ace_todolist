b7c69f8602cbba6f58fb90e3056dbff3eb765ad2
DESKTOP-8BLNMV2\김태윤 <dracpart@gmail.com>
2018-05-27 17:18:48 +0900

    에딧기능중 변경할 항목 입력기능 추가

--- edit.py
+++ edit.py
@@ -1,17 +1,36 @@
 import sqlite3
+import list_todo
 
 
 def edit():
-    conn = sqlite3.connect("lab.db")
+    conn = sqlite3.connect("ace.db")
     cur = conn.cursor()
-    id = input("Record id ?")
-    todo = input("Todo ?")
-    due = input("Due date ?")
-    fin = input("Finished (1: yes, 0: no)?")
+
+    # 수정 전 리스트로 보여주기
+    list_todo.list_todo()
+    print('\n')
+
+    # 변경할 항목 선택
+    whattodo = input("What do you want to edit? \n Record ID => ID\n Todo => TODO\n Due date => DUE\n Finished => FIN")
+
+    #변경내용 작성
+    if 'ID' in  whattodo :
+        id = input("Record id ? ")
+    if 'TODO' in  whattodo :
+        todo = input("Todo ? ")
+    if 'DUE' in  whattodo :
+        due = input("Due date ? ")
+    if 'FIN' in  whattodo :
+        fin = input("Finished (1: yes, 0: no)? ")
+
     sql = "UPDATE todo SET what=?, due=?, finished=? WHERE id=?"
     data = (
-        (id, todo, due, fin),
+        (todo, due, fin, id),
     )
+
     cur.executemany(sql, data)
     conn.commit()
     conn.close()
+
+    # 개행을 위한 print
+    print()

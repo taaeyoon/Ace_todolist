926633cff3895dd2853cfff8aa6254c95dfeb032
Ochodus <mario1470@naver.com>
2018-05-30 04:20:59 +0900

    데이터베이스 출력 기능 업데이트 및 due의 입력 형태에 대한 설정

--- edit_todo.py
+++ edit_todo.py
@@ -1,36 +1,32 @@
 import sqlite3
+import list_todo
+
 
 def edit():
     conn = sqlite3.connect("ace.db")
     cur = conn.cursor()
 
     # 수정 전 리스트로 보여주기
-    sql =" select * from todo where 1"
-    cur.execute(sql)
-    rows = cur.fetchall()
-    print("ID  TODO   DATE   FINISHED? \n---------------------------")
-    for row in rows:
-        ln = row[0]
-        print(row[0],row[1],row[2],row[3])
+    list_todo.list_all()
 
-    #변경할 리스트항목의 넘버
+    # 변경할 리스트항목의 넘버
     id = input("Record id ? ")
-    while int(id) > ln or int(id) < 1 :
-        id = input("Record id ? ")
 
     # 변경할 카테고리 선택
-    whattodo = input("What do you want to edit? \n Todo => TODO\n Due date => DUE\n Finished => FIN\n : ")
+    whattodo = input("What do you want to edit? \n title => TITLE\n "
+                     "Due date => DUE\n Category => CAT\n Order => ORDER\n "
+                     "Finished => FIN\n : ")
 
-    #변경내용 작성
-    if 'TODO' in  whattodo :
+    # 변경내용 작성
+    if 'TODO' in whattodo:
         todo = input("Todo ? ")
-        sql = "UPDATE todo SET what=? WHERE id=?"
+        sql = "UPDATE todo SET title=? WHERE id=?"
         data = (
             (todo, id),
         )
         cur.executemany(sql, data)
 
-    if 'DUE' in  whattodo :
+    if 'DUE' in whattodo:
         due = input("Due date ? ")
         sql = "UPDATE todo SET due=? WHERE id=?"
         data = (
@@ -38,7 +34,23 @@ def edit():
         )
         cur.executemany(sql, data)
 
-    if 'FIN' in  whattodo :
+    if 'CAT' in whattodo:
+        cat = input("Category? ")
+        sql = "UPDATE todo SET category=? WHERE id=?"
+        data = (
+            (cat, id),
+        )
+        cur.excutemany(sql, data)
+
+    if 'ORDER' in whattodo:
+        order = input("Order? ")
+        sql = "UPDATE todo SET priority=? WHERE id=?"
+        data = (
+            (order, id),
+        )
+        cur.executemany(sql, data)
+
+    if 'FIN' in whattodo:
         fin = input("Finished (1: yes, 0: no)? ")
         sql = "UPDATE todo SET finished=? WHERE id=?"
         data = (
@@ -46,13 +58,7 @@ def edit():
         )
         cur.executemany(sql, data)
 
-#변경된 리스트 출력
-    sql =" select * from todo where 1"
-    cur.execute(sql)
-    rows = cur.fetchall()
-    print("ID  TODO   DATE   FINISHED? \n---------------------------")
-    for row in rows:
-        print(row[0],row[1],row[2],row[3])
-
     conn.commit()
+    # 변경된 리스트 출력
+    list_todo.list_all()
     conn.close()

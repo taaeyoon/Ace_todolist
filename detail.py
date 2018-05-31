78a4c087100d2f828b7f0dc28fb40afa2d94a2e2
gunoong011 <gunoong011@gmail.com>
2018-05-31 22:57:56 +0900

    detail.py is updated

--- detail.py
+++ detail.py
@@ -7,7 +7,7 @@ def detail() :
     conn = sqlite3.connect("ace.db")
     cur = conn.cursor()
 
-    col_list = ["id","title","due","category","priority","place","comment","finished"]
+    col_list = ["id","title","category","priority","due","place","comment","finished"]
 
     # 세부사항을 보고 싶은 항목의 id 선택하기
     detail_id = input("choose id of item that you want to see details: ")
@@ -19,7 +19,7 @@ def detail() :
 
     # 항목 보여주기
     for i in range(0,len(col_list)):
-        print(col_list[i],": ",row[i])
+        print(col_list[i],":",row[i])
 
     print()
 

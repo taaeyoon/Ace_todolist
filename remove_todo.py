a8ae217b12824f0151de35ac26645dbbed1caadb
최하은 <cothmm@gmail.com>
2018-06-01 18:12:29 +0900

    edit, remove

--- remove_todo.py
+++ remove_todo.py
@@ -14,8 +14,13 @@ def remove_todo():
         search.search()
 
         sel_id = input("Record id? ")
+        while not sel_id.isdigit():
+            sel_id = input("Record id? ")
         sql = "delete from todo where id = ?"
         cur.execute(sql, (sel_id, ))
+
+        # 제거된 항목 뒤의 id를 앞으로 1씩 당기기
+        cur.execute("update todo set id = id-1 where id > ?", (sel_id))
         conn.commit()
 
         loop = input("\nanything else you want to remove?/n yes:y no:n ")
